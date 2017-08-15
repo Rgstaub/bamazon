89332d9f3eceb9cfcdf6eb6cfe3ac3aa15b244a0
Rgstaub <rgstaub@gmail.com>
2017-08-15 00:44:01 -0400

    bamazonCustomer.js is complete. Clean code with comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,51 +14,97 @@ let displayProducts = () => {
   db.query('SELECT * FROM products', (err, res) => {
     if (err) throw err;
     // Create an array of all product names to be presented to the user
-    res.forEach( (item) => {
-      productArray.push(item.product_name);
-    })
+    res.forEach((item) => productArray.push(item.product_name));
     inquirer.prompt({
       type: 'list',
-      name: 'productList',
+      name: 'productList', 
       message: '\nPlease make a selection from the available products\n',
       choices: productArray
     }).then( (product) => {
+      // continue to display details for selected item
       displayDetails(product.productList);
     })
   }) 
 }
 
-// Take the user selection and display the cost. Prompt user for quantity to purchase
+// Take the user selection and display the cost, stock, and department. Prompt user for quantity to purchase
 let displayDetails = (product) => {
   console.log(`\n==========| ${product} |==========`);
   db.query('SELECT price, department_name, stock_quantity FROM products WHERE ?',
     {product_name: product},
     (err, res) => {
       if (err) throw err;
-      console.log(`Department: ${res[0].department_name}\nPrice: $${res[0].price}\nAvailavle Quantity: ${res[0].stock_quantity}\n`);
-  
+      // variables for readability
+      let dept = res[0].department_name;
+      let price = res[0].price;
+      let stock = res[0].stock_quantity;
+      // Display the item details
+      console.log(`Department: ${dept}\nPrice: $${price}\nAvailavle Quantity: ${stock}\n`);
+      // get the quantity desired from the user
       inquirer.prompt({
         type: 'input',
         name: 'purchaseQty',
         message: "How many would you like to purchase? (enter 0 to return to the products list)",
-        validate: function(qty) {
-          num = Number.parseFloat(qty);
-
-          if (!Number.isInteger(num) || num < 0) {
-            return false;
-          } else if (num > res[0].stock_quantity) {
-            console.log(`\n\n-=- You can't order that many! Check the current stock (${res[0].stock_quantity}) and try again.\n`)
+        // Validate for only positive integers and 0
+        validate: function(input) {
+          num = Number.parseFloat(input);
+          if (!Number.isInteger(num) || num < 0) return false;
+          else if (num > stock) {
+            console.log(`\n\n-=- You can't order that many! Check the current stock (${stock}) and try again.\n`)
             return false;
           } else return true;
         }
       })
-      .then( (qty) => {
-        let orderQty = Number.parseInt(qty.purchaseQty);
-        if ( orderQty === 0) {
-          return displayProducts();
-        }
-        console.log(`Order Complete!\n\n==========| Invoice |==========\nItem: ${product}\nPrice: $${res[0].price}\nQuantity: ${orderQty}\n-----------------\nPurchase Total: ${res[0].price * orderQty}`);
+      .then((input) => {
+        let orderQty = Number.parseInt(input.purchaseQty);
+        // Go back for qty=0
+        if ( orderQty === 0) return displayProducts();
+        // Continue to confirm purchase for valid quantities
+        else confirmPurchase(product, stock, orderQty, price);
       })
     }  
+  )
+}
+
+// just like it sounds...
+let confirmPurchase = function(product, stock, qty, price) {
+  // Make sure this number is pretty
+  let totalPrice = Math.round((price*qty)*100)/100;
+  inquirer.prompt({
+    type: 'confirm',
+    name: 'confirm',
+    message: `\nAre you sure you want to purchase (${qty}) '${product}' for a total of ${totalPrice}?`
+  }).then((confirm) => {
+    // Update the inventories
+    if (confirm.confirm) updateStock(product, stock, qty, price, totalPrice);
+    // or else return to the product details screen
+    else displayDetails(product);
+  })
+}
+
+// complete the transaction by updating inventories in the DB and presenting invoice to user
+let updateStock = function(product, stock, qty, price, totalPrice) {
+  db.query(`UPDATE products SET ? WHERE ?`,
+    [{stock_quantity: stock-qty},
+    {product_name: product}],
+    (err, res) => {
+      if (err) throw err;
+      else {
+        console.log("\nPurchase confirmed.\n");
+        console.log(`Order Complete!\n\n==========| Invoice |==========\nItem: ${product}\nPrice: $${totalPrice}\nQuantity: ${qty}\n-----------------\nPurchase Total: ${totalPrice}`);
+        // See if they want to spend more money. Kick them out if not
+        inquirer.prompt({
+          type: 'confirm',
+          name: 'continue',
+          message: '\nContinue Shopping?'
+        }).then((choice) => {
+          if (choice.continue) displayProducts();
+          else {
+            console.log("\nGoodbye!\n");
+            db.end();
+          }
+        })
+      }
+    }
   )
 }
